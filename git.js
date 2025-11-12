d318524ba4cedf98b3f5e0004d698e55969aaf22
aska diliya <askadiliyan@gmail.com>
2025-11-12 11:01:24 +0700

    feat[]: penampilan nama

---
+++ git.js
@@ -0,0 +1,10 @@
+// // Git Basics
+// git working directori -> staging->
+// git init untuk membuat directory
+// git status melihat working directory
+// git add untuk merubah sementara dari working directory kedalam staging area(bisa menyimpan directori file atau folder)
+// git commit membuat perubahan sementara menjadi permanen dari staging area ke local()
+// git rm --cached (meremove dari staging area)
+// git commit atau git commit -m "tulis pesan commitnya" untuk menyimpan semua yang ada di staging menjadi permanen
+const nama = "aska"
+console.log(nama)
